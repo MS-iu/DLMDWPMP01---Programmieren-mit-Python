d0a6a9d535710e1b2b3ed9568706cd7f97b49ff0
MS-iu <120411565+MS-iu@users.noreply.github.com>
2022-12-19 09:57:02 +0100

    Laden der Datensätze

--- main.py
+++ main.py
@@ -5,3 +5,22 @@ import numpy as np
 import os
 import math
 
+#Erster Test
+print('Diese Lösung wurde mit folgenden Versionen erstellt:')
+print('sqlalchemy: ', db.__version__)
+print('pandas: ', pd.__version__)
+print('matplotlib: ', plt.__version__)
+
+#Abruf Laufwerkspfad
+pfad = os.path.dirname(__file__)
+print ("Verwendeter Laufwerkspfad:", pfad)
+
+#Einlesen der Dateien
+ideal = pd.read_csv("/Users/micha/Documents/GitHub/DLMDWPMP01---Programmieren-mit-Python/ideal.csv")
+training = pd.read_csv("/Users/micha/Documents/GitHub/DLMDWPMP01---Programmieren-mit-Python/train.csv")
+test = pd.read_csv("/Users/micha/Documents/GitHub/DLMDWPMP01---Programmieren-mit-Python/test.csv")
+
+print(ideal)
+print(training)
+print(test)
+
